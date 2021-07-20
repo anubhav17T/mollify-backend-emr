23a3ff9770f779adddef1cd3ec8b32bd02924413
Anubhav Tyagi <anubhav1tyagi@gmail.com>
2021-07-20 20:07:52 +0530

    return message check

--- utils/utils_classes/consultation_return_message.py
+++ utils/utils_classes/consultation_return_message.py
@@ -2,32 +2,39 @@ from fastapi import status
 
 
 class ConsultationStatusMessage:
-    def __init__(self, status: str):
+    def __init__(self, status: str,id:int):
         self.status = status
+        self.id = id
+
 
     def message(self):
         if self.status == "OPEN":
             return {"message": "CHEERS!! Your consultation has been booked",
                     "code": status.HTTP_201_CREATED,
+                    "id":self.id,
                     "success": True}
 
         if self.status == "INPROGRESS":
             return {"message": "Now Your consultation Is In Progress,have a great session (Note:- ConsultationCannot "
                                "be cancelled)",
                     "code": status.HTTP_200_OK,
+                    "id": self.id,
                     "success": True}
 
         if self.status == "COMPLETED":
             return {"message": "CONGRATULATIONS !! Your consultations has been completed successfully",
                     "code": status.HTTP_200_OK,
+                    "id": self.id,
                     "success": True}
 
         if self.status == "CANCELLED":
             return {"message": "We Regret that you have cancelled the confirmation",
                     "code": status.HTTP_200_OK,
+                    "id": self.id,
                     "success": True}
 
         if self.status == "RESCHEDULED":
             return {"message": "Your Confirmation has been rescheduled to your preferrable time",
                     "code": status.HTTP_200_OK,
+                    "id": self.id,
                     "success": True}
